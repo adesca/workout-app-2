bfef112f121fa8f4045b86bd4ab47007f05ddd89
adesca <adeolubabatunde@gmail.com>
2026-06-02 18:03:48 -0500

    Enable deploys

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tanstackRouter from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      tanstackRouter({
-      target: 'react',
-      autoCodeSplitting: true
-    }),
-    react()
-  ],
-  optimizeDeps: {
-    exclude: ['@electric-sql/pglite']
-  }
+    base: '/workout-app-2',
+    plugins: [
+        tanstackRouter({
+            target: 'react',
+            autoCodeSplitting: true
+        }),
+        react()
+    ],
+    optimizeDeps: {
+        exclude: ['@electric-sql/pglite']
+    }
 })
